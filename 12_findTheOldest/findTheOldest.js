100f1e952d535b2ecba3677871c79236d2d49ae5
willelol04 <147752701+willelol04@users.noreply.github.com>
2024-03-29 15:41:42 +0100

    complete findTheOldest-exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    array.sort((a, b) => {
+        
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+
+        if(!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        
+
+        if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return -1;
+        }
+        return 1;
+    });
+
+    return(array[0]);
 
 };
 
+
 // Do not edit below this line
+
+
 module.exports = findTheOldest;

abf0051fc490a731fa07202aac9fab3ea619078a
willelol04 <147752701+willelol04@users.noreply.github.com>
2024-03-29 14:57:04 +0100

    complete fibonacci-exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    sum = 0;
+    last = 1;
+    if(!(num >= 0)) {
+        return "OOPS";
+    }
+
+    for(let i = 1; i <= num; i++) {
+        sum += last;
+        last = sum - last;
+        
+        
+    }
+
+    return(sum);
 
 };
 
